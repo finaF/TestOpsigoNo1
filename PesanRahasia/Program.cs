f0bb923bbc72dd45f31fcfb16e19344ecc2462d2
FinaFerina <finaferina9@gmail.com>
2021-07-18 23:03:01 +0700

    Add project files.

---
+++ PesanRahasia/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PesanRahasia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NO 1 : PESAN RAHASIA");
+            Console.WriteLine("Input : ");
+            string input = Console.ReadLine();
+            pesan_rahasia(input);
+            Console.ReadLine();
+        }
+
+        static void pesan_rahasia(string input)
+        {
+            int L = input.Length;
+            while (L > 1000)
+            {
+                Console.WriteLine("Pesan tidak boleh lebih dari 1000 karakter!");
+                Console.WriteLine("Input : ");
+                input = Console.ReadLine();
+                L = input.Length;
+            }
+            double result = Math.Sqrt(L);
+            int K = Convert.ToInt32(result), M = 0, count = 0;
+            bool isSquare = result % 1 == 0;
+            if (!isSquare)
+            {
+                K = (int)result + 1;
+            }
+            M = K * K;
+            for (int i = 0; i < M - L; i++)
+            {
+                input += "*";
+            }
+            char[] arr = input.ToCharArray();
+            char[,] inputan = new char[K, K];
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = 0; j < K; j++)
+                {
+                    inputan[i, j] = arr[count];
+                    count++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Output : ");
+            for (int i = 0; i < K; i++)
+            {
+                for (int j = K - 1; j >= 0; j--)
+                {
+                    if (inputan[j, i] != '*')
+                        Console.Write(inputan[j, i]);
+                }
+            }
+        }
+    }
+}
---
+++ SusunanAngka/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SusunanAngka
+{
+    class Program
+    {
+        static void cariSusunanAngka(int[] array, int index, int num, int sisa)
+        {
+            if (sisa < 0)
+                return;
+
+            if (sisa == 0)
+            {
+                for (int i = 0; i < index; i++)
+                    Console.Write(array[i] + " ");
+                Console.WriteLine();
+                return;
+            }
+
+            int prev = (index == 0) ?
+                                1 : array[index - 1];
+
+            for (int k = num; k >= prev; k--)
+            {
+                array[index] = k;
+                cariSusunanAngka(array, index + 1, num,
+                                        sisa - k);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("NO 2 : SUSUNAN ANGKA");
+            Console.Write("Input a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[num];
+            cariSusunanAngka(array, 0, num, num);
+            Console.ReadLine();
+        }
+    }
+}
